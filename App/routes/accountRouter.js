1d724af4b79d5d38b2f496da4c2c84b4b277b735
Maxim Salikhov <msalihov@gmail.com>
2019-03-16 22:04:58 +0800

    Migrate login actions to accountRouter

--- App/routes/accountRouter.js
+++ App/routes/accountRouter.js
@@ -1,6 +1,46 @@
 const express = require("express");
 const router = express.Router();
-const { checkUserLoggedIn } = require("./middleware/auth");
+const passport = require("passport");
+
+const {
+  checkLoggedIn,
+  checkLoggedOut,
+  checkUserLoggedIn
+} = require("./middleware/auth");
+
+/* =====================================
+   ========== AUTHENTICATION ===========
+   ===================================== */
+
+/* GET login - Login Form */
+router.get("/login", checkLoggedOut, function(req, res, next) {
+  res.render("account/login", {
+    title: "Login",
+    navCat: "login",
+    loggedIn: req.user
+  });
+});
+
+/* POST login - Login Action */
+router.post(
+  "/login",
+  checkLoggedOut,
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true
+  }),
+  function(req, res) {
+    req.flash("success", "You have successfully logged in!");
+    res.redirect("/");
+  }
+);
+
+/* GET/POST logout - Logout Action */
+router.all("/logout", checkLoggedIn, function(req, res, next) {
+  req.logout();
+  req.flash("success", "You have been logged out!");
+  res.redirect("/");
+});
 
 router.get("/refer", checkUserLoggedIn, function(req, res, next) {
   res.render("account/refer", {
--- App/routes/indexRouter.js
+++ App/routes/indexRouter.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
-const { checkLoggedIn, checkLoggedOut } = require("./middleware/auth");
 
 /* Index Page */
 router.get("/", function(req, res, next) {
@@ -11,30 +9,5 @@ router.get("/", function(req, res, next) {
     loggedIn: req.user
   });
 });
-
-/* Logout */
-router.get("/logout", checkLoggedIn, function(req, res, next) {
-  req.logout();
-  req.flash("success", "You have been logged out!");
-  res.redirect("/");
-});
-
-/* Login Form */
-router.get("/login", checkLoggedOut, function(req, res, next) {
-  res.render("login", { title: "Login", navCat: "login", loggedIn: req.user });
-});
-
-router.post(
-  "/login",
-  checkLoggedOut,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true
-  }),
-  function(req, res) {
-    req.flash("success", "You have successfully logged in!");
-    res.redirect("/");
-  }
-);
 
 module.exports = router;
