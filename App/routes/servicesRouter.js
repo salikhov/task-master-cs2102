e1aef810aa6aff93c09061e4c494d53adc11fd48
Maxim Salikhov <msalihov@gmail.com>
2019-04-06 20:27:50 +0800

    Add search functionality to services page

--- App/routes/servicesRouter.js
+++ App/routes/servicesRouter.js
@@ -5,15 +5,49 @@ const pool = require("../db");
 
 /* Services homepage */
 router.get("/", function(req, res, next) {
-  pool.query("select serviceId, name, description from services", function(
-    err,
-    data
-  ) {
-    res.render("services/services", {
-      title: "Services",
-      navCat: "services",
-      services: data.rows,
-      loggedIn: req.user
+  let query =
+    "select serviceId, S.name as sname, description, price, R.name as rname, C.name as cname from services S " +
+    "join cityregions R on R.regionid = S.regionid join categories C on C.catid = S.catid ";
+  const params = [];
+  if (req.query.q || req.query.p || req.query.c || req.query.r) {
+    query += "where ";
+    if (req.query.q) {
+      query += "(S.name ILIKE $1 or S.description ILIKE $2)";
+      params.push("%" + req.query.q + "%");
+      params.push("%" + req.query.q + "%");
+    }
+    if (req.query.p) {
+      const paramName = "$" + (params.length + 1);
+      if (params.length !== 0) query += " and ";
+      query += "price <= " + paramName;
+      params.push(req.query.p);
+    }
+    if (req.query.c) {
+      const paramName = "$" + (params.length + 1);
+      if (params.length !== 0) query += " and ";
+      query += "S.catid=" + paramName;
+      params.push(req.query.c);
+    }
+    if (req.query.r) {
+      const paramName = "$" + (params.length + 1);
+      if (params.length !== 0) query += " and ";
+      query += "S.regionid=" + paramName;
+      params.push(req.query.r);
+    }
+  }
+  pool.query(query, params, function(err, data) {
+    pool.query("select * from categories", function(err, data2) {
+      pool.query("select * from cityregions", function(err, data3) {
+        res.render("services/services", {
+          title: "Services",
+          navCat: "services",
+          services: data.rows,
+          categories: data2.rows,
+          regions: data3.rows,
+          query: req.query,
+          loggedIn: req.user
+        });
+      });
     });
   });
 });
